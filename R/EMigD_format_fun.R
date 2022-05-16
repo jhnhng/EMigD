fee9ff97a4bfe5cea682e65cb69551b25de85f12
John Huang <john.huang5595@gmail.com>
2022-05-16 10:58:39 -0600

    EMigD_format function added

---
+++ R/EMigD_format_fun.R
@@ -0,0 +1,34 @@
+#' Re-formats the User's data frame for the EMigD workflow
+#'
+#' @param dat A data frame 
+#' @param id_ The individual ID column in 'dat'. This column should be a 'factor'.
+#' @param x_  The x-coordinate column in 'dat'. 
+#' @param y_  The y-coordinate column in 'dat'.
+#' @param year_ The column containing the years in 'dat'. The column should be 
+#' a numeric value.
+#' @param month_ The column containing the months in 'dat'. The column should 
+#' be a numeric value.
+#' @param jdate_ The column containing the julian dates. This column should be 
+#' a numeric value
+
+#' @return Function re-formats the user's data frame for the EmigD workflow 
+#'
+
+#' @export
+EmigD_format <- function(dat, id_, x_, y_, year_, month_, jdate_){
+  EMigD_cols <- subset(dat, 
+                       select=c(id_,
+                                x_, 
+                                y_, 
+                                year_, 
+                                month_, 
+                                jdate_))
+  
+  names(EMigD_cols) <- c("id_",
+                            "x_", 
+                            "y_", 
+                            "year_", 
+                            "month_", 
+                            "jdate_" )
+  return(EMigD_cols)
+}
